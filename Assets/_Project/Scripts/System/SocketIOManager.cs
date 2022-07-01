0926cd524bdff749793b64519d43984e42f5817b
bas-interactive <benjamin.seitter@icloud.com>
2022-07-01 14:45:58 +0200

    Made SocketIO working

--- Assets/_Project/Scripts/System/SocketIOManager.cs
+++ Assets/_Project/Scripts/System/SocketIOManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using SocketIOClient;
 using System;
 using SocketIOClient.Newtonsoft.Json;
+using System.Net;
+using System.IO;
 
 namespace snakefever
 {
@@ -16,7 +18,31 @@ namespace snakefever
 
         public void Init()
         {
-            Socket = new SocketIOUnity(Uri, new SocketIOOptions{});
+            // string html = string.Empty;
+            // string url = @"http://snakefever.glitch.me";
+
+            // HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            // request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            // using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            // using (Stream stream = response.GetResponseStream())
+            // using (StreamReader reader = new StreamReader(stream))
+            // {
+            //     html = reader.ReadToEnd();
+            // }
+
+            // Debug.Log(html);
+
+            // return;
+
+
+            Socket = new SocketIOUnity(Uri, new SocketIOOptions{
+                EIO = 4,
+                ExtraHeaders = new Dictionary<string, string>() {
+                    {"User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36 OPR/88.0.4412.53"}
+                }
+            });
 
             Socket.JsonSerializer = new NewtonsoftJsonSerializer();
 
